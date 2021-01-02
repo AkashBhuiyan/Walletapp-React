c3712074ccf0758ab8429ba736456478e2db8b89
AkashBhuiyan <akash.bhuiyan@gmail.com>
2021-01-02 18:39:31 +0600

    Project design and component added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/shared/Header';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './components/Home';
+import Dashboard from './components/dashboard/Dashboard';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Route path="/" component={Header}></Route>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/dashboard" component={Dashboard}/>
+          </Switch>
+    </Router>
   );
 }
 
