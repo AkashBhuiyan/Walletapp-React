cecb31adb5a9eafdfaa64e9499b62a6fe9beabde
AkashBhuiyan <akash.bhuiyan@gmail.com>
2021-01-03 22:38:56 +0600

    Store and Reducer added for redux

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import Home from './components/Home';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateWallet from './components/dashboard/CreateWallet';
 import NotFound from './components/shared/NotFound';
+import {Provider} from 'react-redux';
+import store from './Store';
 
 
 function App() {
   return (
-    <Router>
-        <Route path="/" component={Header}></Route>
-            <Switch>
-                <Route path="/" exact component={Home}/>
-                <Route path="/dashboard" component={Dashboard}/>
-                <Route path="/createwallet" component={CreateWallet}/>
-                <Route path="/" component={NotFound}/>
-          </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+          <Route path="/" component={Header}></Route>
+              <Switch>
+                  <Route path="/" exact component={Home}/>
+                  <Route path="/dashboard" component={Dashboard}/>
+                  <Route path="/createwallet" component={CreateWallet}/>
+                  <Route path="/" component={NotFound}/>
+            </Switch>
+      </Router>
+    </Provider>
   );
 }
 
